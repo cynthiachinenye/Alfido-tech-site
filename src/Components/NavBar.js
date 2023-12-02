7b7a2d87bc7a9488241d5f7d46684b4c44ab11b7
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-02 17:49:28 +0100

    new

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,23 +6,24 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const NavBar = () => {
   return (
-    <section id="nav" className='block block-header'>
-
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary  ">
     <Container>
-      <Navbar.Brand href="#home"><img src=".././Assets/Alfido.png" alt='logo'/></Navbar.Brand>
+      <Navbar.Brand href="#home"><img src=".././Assets/Alfido.png" alt='logo' className='logo '/></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Collapse id="basic-navbar-nav " className='ps-5'>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="internship" className='active text-warning'>Internship</Nav.Link>
+          <Nav.Link href="#link">Workshop</Nav.Link>
+          <Nav.Link href="#link">Submission</Nav.Link>
+          <Nav.Link href="#link">Verify Certificate</Nav.Link>
          
         </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
       
-    </section>
+  
   )
 }
 
