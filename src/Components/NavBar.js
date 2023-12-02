227cc610e3dcdc43a2c3cf619ba57da3747a7c72
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-02 15:03:40 +0100

    new

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
 
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home"><img src=".././Assets/Alfido.png" alt='logo'/></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
