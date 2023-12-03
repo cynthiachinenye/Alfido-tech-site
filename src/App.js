13a4232b8cfa438da517d86d3c89906a4b511491
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-03 22:02:18 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Home from './Components/Home';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <header id="header">
        <NavBar/>
       </header>
+      <main>
+       <Home/>
+      </main>
     </div>
   );
 }
