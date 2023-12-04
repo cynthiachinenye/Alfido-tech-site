eb22b9e01a4809df285230605c9d9c62d885bbbe
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-04 22:59:49 +0100

    new

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
 
 const Home = () => {
   return (
-    <div>
-      
+    <section className='block block-home'>
+    <Container fluid>
+    <div className='text-center m-5 content'>
+    <p  className='fs-4 section-title'><span></span> contact us <span></span></p>
+  
+    <h1>Internship Program Details</h1>
     </div>
+    <div>
+    <div>
+    <p></p>
+    <ul>
+    <li><strong>Duration: 1 Month</strong></li>
+    <li><strong>Starting Date:</strong><strong> 10 December 2023</strong></li>
+    <li><strong>Submission Date:</strong><strong> 25 December To 10 January</strong></li>
+    <li><strong>Mode:</strong><strong> Online & Task Based</strong></li>
+    <li><strong>Perk:</strong><strong> Offer letter  & Completion Certification</strong></li>
+    <li><strong>Domains:</strong><strong> 5+</strong></li>
+    
+
+    
+    </ul>
+    <hr/>
+    
+    </div>
+    </div>
+    
+    </Container>
+
+      
+    </section>
   )
 }
 
