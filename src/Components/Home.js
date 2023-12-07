0322e1a272d0c3996cf6fe47040d702e7217fc61
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-07 11:53:34 +0100

    new

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -26,9 +26,20 @@ const Home = () => {
     </ul>
     <hr className='horizon'/>
     <div>
-    <p>📌 Apply now and get offer letter within 24 Hours</p>
-    <h2 className='text-dark fs-5 fw-700'>📌 Join Alfido Tech Communities</h2>
+    <p className='text-danger fw-100'>📌 Apply now and get offer letter within 24 Hours</p>
+    <h2 className='text-dark fs-4 fw-700 ps-3'>📌 Join Alfido Tech Communities</h2>
+    <ul>
+    <li>LinkedIn:- <button className=' btn btn-outline-dark btn-light fs-5 rounded-pill'>Click Here</button></li>
+    <li>Alfido Tech community:- <button className='btn btn-outline-dark btn-light fs-5 rounded-pill'>Click Here</button></li>
+    <li>Alfido Tech:- <button className='btn btn-outline-dark btn-light fs-5 rounded-pill'>Click Here</button></li>
+    <li>Telegram:- <button className='btn btn-outline-dark btn-light fs-5 rounded-pill'>Click Here</button></li>
+    </ul>
     </div>
+    <hr className='horizon'/>
+
+    <h2 className='text-dark fs-4 fw-700 ps-3 w-100'>📌 Please carefully fill out all of the details that will be needed for the internship offer letter and internship certificate.
+    If You Want a 4-month internship certificate, I will Give You some conditions.</h2>
+    
     </div>
     </div>
     
